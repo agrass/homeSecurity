1d7376e74c07e34b10c002c5d9718791eefaf3b0
Antonio Grass <antonio@antonio-HP-Pavilion-dm4-Notebook-PC.(none)>
2014-04-24 13:17:58 -0300

    rest client add

--- app/models/ip_config.rb
+++ app/models/ip_config.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'rest_client'
 class IpConfig < ActiveRecord::Base
   attr_accessible :ip
 
@@ -9,19 +10,19 @@ class IpConfig < ActiveRecord::Base
     UserFriend.find_all_by_user_id(user_id).each do |friend|
       ids2 << Device.find(User.find(friend.friend_id).device_id).key
     end
-    @result = HTTParty.post("https://android.googleapis.com/gcm/send", 
-        :body => {
+    @result = RestClient.post("https://android.googleapis.com/gcm/send", 
+        {
                   :registration_ids => ids1,
                   :data => {:message => msg }   
                   }.to_json,
-        :headers => { 'Content-Type' => 'application/json', 'Authorization' => 'key=AIzaSyAnvwkIdKFjR65RMRx5vEekA_BZVe4kXK8' } )
+         { 'Content-Type' => 'application/json', 'Authorization' => 'key=AIzaSyAnvwkIdKFjR65RMRx5vEekA_BZVe4kXK8' } )
   
-  @result2 = HTTParty.post("https://android.googleapis.com/gcm/send", 
-        :body => {
+  @result2 = RestClient.post("https://android.googleapis.com/gcm/send", 
+         {
                   :registration_ids => ids2,
                   :data => {:message => "Dispositivo " + User.find(user_id).name + ": " + msg  }   
                   }.to_json,
-        :headers => { 'Content-Type' => 'application/json', 'Authorization' => 'key=AIzaSyAnvwkIdKFjR65RMRx5vEekA_BZVe4kXK8' } )
+         { 'Content-Type' => 'application/json', 'Authorization' => 'key=AIzaSyAnvwkIdKFjR65RMRx5vEekA_BZVe4kXK8' } )
   end
 
 end
